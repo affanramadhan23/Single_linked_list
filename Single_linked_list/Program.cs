fca442b330df0456cd8d9b3609a4f61d49644238
affanramadhan23 <affannabilramadhan@gmail.com>
2022-11-11 19:46:23 +0700

    add class program,static void with list obj and add switch case 1 & 2

--- Single_linked_list/Program.cs
+++ Single_linked_list/Program.cs
@@ -112,4 +112,57 @@ namespace singly_linked_list
                 return false;
         }
     }
+
+    class Program
+    {
+
+        //check wheter the specified node is present in the list or not
+
+        static void Main(string[] args)
+        {
+            List obj = new List();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMENU");
+                    Console.WriteLine("1. Add a record to the list ");
+                    Console.WriteLine("2. Delete a record from the list ");
+                    Console.WriteLine("3. View all the records in the list ");
+                    Console.WriteLine("4. Search for a second in the list ");
+                    Console.WriteLine("5. EXIT");
+                    Console.Write("\nEnter your choice (1-5) : ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNote();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Console.Write("\nEnter the roll number of" + "the student whose record is to be deleted :");
+                                int nim = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.delNode(nim) == false)
+                                    Console.WriteLine("\n Record not found.");
+                                else
+                                    Console.WriteLine("Record with roll number " + nim + "Deleted");
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.traverse();
+                            }
+                    }
+                }
+            }
+        }
+    }
 }
