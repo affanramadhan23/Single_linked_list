2081b55d8fc75e9c8776723e04fedceced6fe310
affanramadhan23 <affannabilramadhan@gmail.com>
2022-11-11 17:20:19 +0700

    add class list

--- Single_linked_list/Program.cs
+++ Single_linked_list/Program.cs
@@ -11,4 +11,28 @@ namespace singly_linked_list
         public string name;
         public Node next;
     }
+    class List
+    {
+        Node START;
+        public List()
+        {
+            START = null;
+        }
+        public void addNote() // add a node in the list
+        {
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number of the student : ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student : ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = nim;
+            newnode.name = nm;
+            if (START != null || nim <= START.rollNumber)
+            {
+
+            }
+        }
+    }
 }
