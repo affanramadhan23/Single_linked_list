1e1c7eeb18eb35f45017739591d092e7d737ea6e
affanramadhan23 <affannabilramadhan@gmail.com>
2022-11-11 17:11:58 +0700

    create class node

--- Single_linked_list/Program.cs
+++ Single_linked_list/Program.cs
@@ -1,1 +1,14 @@
-﻿using system;
+﻿using System;
+
+
+namespace singly_linked_list
+{
+    //each node consist of the information part and link to the next mode
+
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+}
